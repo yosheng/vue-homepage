5c3c6b29a30d99f965faeeaaabee259f3e5a5ae1
risenzhang <sad445566@gmail.com>
2019-12-09 14:50:30 +0800

    添加导览列&移除Logo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Navigation></Navigation>
+    <router-view />
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navigation: Navigation
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Navigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Nav Bar</h2>
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+  </div>
+</template>
